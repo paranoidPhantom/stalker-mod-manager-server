4ea661fbacf8d896674194f3042b90b7289844db
Andrei <andrei.hudalla@outlook.com>
2023-06-16 19:03:53 -0500

    Added authentication functionality

---
+++ bot.js
@@ -0,0 +1,134 @@
+require('dotenv').config()
+const fs = require("fs")
+const colors = require("colors")
+const { Telegraf, Telegram, Format, Input, Markup } = require('telegraf');
+const { Keyboard } = require('telegram-keyboard')
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const JSON_DB = require('simple-json-db');
+const CONFIG = require("./config.json")
+const modsDB = new JSON_DB('db/modDB.json');
+const usersDB = new JSON_DB('db/usersDB.json');
+const sessionsDB = new JSON_DB('db/sessionsDB.json');
+var registeredCommands = []
+var dynamicQueryListeners = []
+
+const regCmd = (command, description, callbackfn, disableRegistration) => {
+    try {
+        if (command == "/start") {
+            bot.start(callbackfn);
+        } else if (command == "/help") {
+            bot.help(callbackfn)
+        } else if (command.startsWith("/")) {
+            bot.command(command.replace("/",""), callbackfn)
+        } else {
+            bot.hears(command,callbackfn)
+        }
+        if (disableRegistration !== true && command.startsWith("/")) {
+            registeredCommands[registeredCommands.length] = {command: command.replace("/",""), description: description}
+        }
+    } catch(err) {
+        console.log(("Error registering '"+command+"' |",err).red)
+    }
+}
+
+const regAction = (action_id, callbackfn) => {
+    try {
+        bot.action(action_id,callbackfn)
+    } catch(err) {
+        console.log(("Error registering action '"+action_id+"' |",err).red)
+    }
+}
+
+//________Command registration__________
+const Buttons = Markup.inlineKeyboard
+const BTN = Markup.button.callback
+
+const funnyPhrases = [
+    "А нууу, подорвали пацаны!",
+    "Братишка, а ты нудный. В морду давно не получал?",
+    "Херачь их, пацаны!",
+    "Кранты вам всем!",
+    "Закопаем вас, сучье!",
+    "Ааа, ща мы вам, арабы недоделанные!",
+    "Заходи, с боку заходи!",
+    "Берем его тепленького!",
+    "Не мандражуй, пацаны, обходим!",
+    "Быро, обходи, обходи эту шелупонь!",
+    "А ну, чики-брики и в дамки!",
+    "Срисовали нас! Шухер!",
+    "Достало мля в натуре уже всё, понты эти еще фраерские...",
+    "Да манал я эту зону, я в город хочу.",
+    "Нету, мля, жизни, в натуре, нигде нету...",
+    "Эхх... Бабу бы... Приголубил бы любую...",
+    "Гоп-стоп, мы подошли из-за угла..."
+]
+
+const generateCode = async (ctx) => {
+    const user = ctx.from
+    let code = ""
+    while (sessionsDB.has("_login_code_"+code) || code == "") {
+        code = ""
+        const length = 6
+        const characters = '0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+          code += characters.charAt(Math.floor(Math.random() * charactersLength));
+          counter += 1;
+        }
+    }
+    const db =  sessionsDB.JSON()
+    const keys = Object.keys(db)
+    keys.forEach((key) => {
+        const checkingUser = db[key]
+        if (checkingUser.id === user.id) {
+            sessionsDB.delete(key)
+        }
+    })
+    sessionsDB.set("_login_code_"+code, user)
+    //
+    await ctx.replyWithDice()
+    await ctx.replyWithMarkdown("***'"+funnyPhrases[Math.floor(Math.random() * funnyPhrases.length)]+"'***")
+    setTimeout(async () => {
+        await ctx.replyWithMarkdown("```"+code+"```")
+    }, 1000);
+}
+
+// Start command
+
+regCmd("/start","🤝 Начало взаимодействия", async (ctx) => {
+    try {
+        await ctx.replyWithHTML("<b>Привет!</b>\n<i>Введи команду /login или нажми кнопку чтобы получить код!</i>",Buttons(
+            [
+                [BTN("🚀","get_code")]
+            ]
+        ))
+    } catch(err) {
+        await ctx.replyWithMarkdown("**Ошибка выполнения комманды:** \n`\n"+err+"\n`")
+        console.log(("Error running '/start' |",err))
+    }
+})
+
+regCmd("/login", "Получить код для входа на сайт", generateCode)
+
+regAction("get_code", generateCode)
+
+//______________________________________
+try {
+    bot.on("callback_query",(ctx) => {
+        const data = ctx.callbackQuery.data
+        dynamicQueryListeners.forEach(listener => {
+            if (data.startsWith(listener.prefix)) {
+                const input = data.replace(listener.prefix,"").split("_")
+                listener.callbackfn.call(this,ctx,input)
+            }
+        })
+    })
+    bot.telegram.setMyCommands(registeredCommands)
+    process.once('SIGINT', () => bot.stop('SIGINT'));
+    process.once('SIGTERM', () => bot.stop('SIGTERM'));
+    bot.launch();
+    console.log("\nBot listening!".rainbow)
+} catch(err) {
+    console.log("ERROR STARTING BOT:".red,err.bgRed)
+}
--- server.js
+++ server.js
@@ -1,56 +1,94 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const JSONdb = require('simple-json-db');
-const modsDB = new JSONdb('db/modDB.json');
-require('dotenv').config()
-const port = process.env.PORT || 3000
+const JSON_DB = require("simple-json-db");
+const modsDB = new JSON_DB("db/modDB.json");
+const colors = require("colors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
+
+function makeSessionID(length) {
+    let result = "";
+    const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        );
+        counter += 1;
+    }
+    return result;
+}
 
 // GET request to get all mods
-app.get('/api/mods', (req, res) => {
-  const mods = modsDB.JSON()["mods"];
-  mods.forEach((mod, id) => {
-    mod.id = id
-  })
-  res.json(mods);
+app.get("/api/mods", (req, res) => {
+    const mods = modsDB.JSON()["mods"];
+    mods.forEach((mod, id) => {
+        mod.id = id;
+    });
+    res.json(mods);
 });
 
 // GET request to get individual mod metadata with a unique ID
-app.get('/api/mods/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  let mod = null
-  const dbCurrent = modsDB.JSON()
-  if (!dbCurrent["mods"]) { modsDB.set("mods", []) }
-  dbCurrent["mods"].forEach((data, elementID) => {
-    if (id === elementID) {
-      mod = data
+app.get("/api/mods/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    let mod = null;
+    const dbCurrent = modsDB.JSON();
+    if (!dbCurrent["mods"]) {
+        modsDB.set("mods", []);
     }
-  });
-  if (mod) {
-    res.json(mod);
-  } else {
-    res.status(404).send('Mod not found');
-  }
+    dbCurrent["mods"].forEach((data, elementID) => {
+        if (id === elementID) {
+            mod = data;
+        }
+    });
+    if (mod) {
+        res.json(mod);
+    } else {
+        res.status(404).send("Mod not found");
+    }
 });
 
 // POST request to add a new mod
-app.post('/api/mods', (req, res) => {
-  const mod = req.body;
-  if (mod) {
-    const mods = modsDB.JSON()["mods"]
-    let id = 0
-    if (mods) { id = mods.length }
-    mods[id] = mod
-    modsDB.set("mods", mods)
-    res.json({ message: 'Mod added successfully!', mod });
-  } else {
-    res.status(400).send('Invalid mod data');
-  }
+app.post("/api/mods", (req, res) => {
+    const mod = req.body;
+    if (mod) {
+        const mods = modsDB.JSON()["mods"];
+        let id = 0;
+        if (mods) {
+            id = mods.length;
+        }
+        mods[id] = mod;
+        modsDB.set("mods", mods);
+        res.json({ message: "Mod added successfully!", mod });
+    } else {
+        res.status(400).send("Invalid mod data");
+    }
+});
+
+app.post("/api/login", async (req, res) => {
+    const sessionsDB = new JSON_DB("db/sessionsDB.json");
+    const { code } = req.query;
+    if (sessionsDB.JSON()[`_login_code_${code}`]) {
+        const sessionID = makeSessionID(120);
+        const user = sessionsDB.get("_login_code_" + code);
+        sessionsDB.delete("_login_code_" + code);
+        sessionsDB.set("_session_" + sessionID, user);
+        res.json({ session: sessionID, user });
+    } else {
+        console.log(`${`_login_code_${code}`} not found...`);
+        res.status(400).send("Invalid auth code");
+    }
 });
 
 app.listen(port, () => {
-  console.clear()
-  console.log(`Server listening on port ${port}`);
+    console.clear();
+    console.log(`Server listening on port ${port}`.rainbow);
 });
+
+// Launch bot
+const BOT = require("./bot.js");
